dffac9d0ed7cd91d4a87fdf9ea240a68fe076c7a
cho_jae_sin <1005jsc@naver.com>
2022-10-17 16:25:53 +0900

    test

--- src/shared/Rounter.js
+++ src/shared/Rounter.js
@@ -1,10 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>테스트</div>} />
+        <Route path='/' element={<div>테스트</div>} />
       </Routes>
     </BrowserRouter>
   );

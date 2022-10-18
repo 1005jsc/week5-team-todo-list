bc2ee153b2e202aa6fb5b73c67c5fc3b71b0fc6a
Yboda <ybr2265@naver.com>
2022-10-18 23:14:42 +0900

    상세페이지 연결

--- src/pages/todo_list/TodoList.jsx
+++ src/pages/todo_list/TodoList.jsx
@@ -2,10 +2,12 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Box, Flex, P, Button } from "../../common";
 import { __delTodos, __getTodos } from "../../redux/modules/todoSlice";
+import { useNavigate } from "react-router-dom";
 
 const TodoList = () => {
   const dispatch = useDispatch();
   const { isLoading, error, todos } = useSelector((state) => state.todos);
+  const Navigate = useNavigate();
 
   useEffect(() => {
     dispatch(__getTodos());
@@ -22,7 +24,12 @@ const TodoList = () => {
     <Flex fd="column">
       <h1>내 할일</h1>
       {todos.map((todo) => (
-        <Box key={todo.id}>
+        <Box
+          key={todo.id}
+          onClick={() => {
+            Navigate(`/todo_detail/${todo.id}`);
+          }}
+        >
           <P mg="10px 0 0 0">{todo.title}</P>
           <P fs="10">작성자 : {todo.user}</P>
           <Button onClick={() => dispatch(__delTodos(todo.id))}>
